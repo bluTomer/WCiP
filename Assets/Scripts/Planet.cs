664383c6555ec0a8de007358a99fbd25f2f4ab0e
Tomer Blu <Tomer.Blushinsky@Scientificgames.com>
2018-01-26 13:34:11 +0200

    Drafts are hot

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+	public float RotationSpeed;
+
+	private Vector3 _parentPos;
+	
+	private void Start()
+	{
+		_parentPos = transform.parent.position;
+	}
+
+	private void Update()
+	{
+		transform.RotateAround(_parentPos, Vector3.forward, RotationSpeed * Time.deltaTime);
+		transform.RotateAround(transform.position, Vector3.forward, (30.0f - RotationSpeed) * Time.deltaTime);
+	}
+}
